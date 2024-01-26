4a8263f11dafd5bc55aababb8a755a4e3e99cb0a
jasurumarov <jasurumarov199@gmail.com>
2024-01-27 02:08:15 +0500

    file structure

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ['./*.html', './pages/*.html'],
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1146px',
+    },
+    extend: {
+      colors: {
+        colorFirst: '#FFC000',
+        colorSecond: '#0D0D0D',
+        colorThird: '',
+        colorFourth: '',
+        colorFifth: '',
+        colorSixth: '',
+        colorSeventh: '',
+        colorEighth: '',
+      },
+      backgroundImage: {
+  
+      },  
+    },
+    fontFamily: {
+      inter: ['Inter'],
+      montserrat: ['Montserrat'],
+    }
+  },
+  plugins: [],
+}
+
