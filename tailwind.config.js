599391eeef5272ea28aa35b6995d160af29b1835
jasurumarov <jasurumarov199@gmail.com>
2024-01-30 04:28:25 +0500

    men's wear page finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,13 @@ module.exports = {
       },
       boxShadow: {
         'headerShadow': '0px 14px 64px -4px rgba(24, 39, 75, 0.12), 0px 8px 22px -6px rgba(24, 39, 75, 0.12)',
+        'menswear': '0px 1px 0px 0px rgba(0, 0, 0, 0.10)'
       }
     },
     fontFamily: {
       inter: ['Inter'],
       montserrat: ['Montserrat'],
+      montserrat600: ['Montserrat600'],
       poppins: ['Poppins'],
       poppins700: ['Poppins700'],
     }
